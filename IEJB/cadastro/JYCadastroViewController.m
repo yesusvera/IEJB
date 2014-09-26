67712ab713e555fd7db291b56f13b70e8cb247e0
alyssonlopes <alysson.als@gmail.com>
2014-09-26 17:58:57 -0300

    - Ajustes no design de cadastro - Next no teclado passa para o próximo campo

--- IEJB/cadastro/JYCadastroViewController.m
+++ IEJB/cadastro/JYCadastroViewController.m
@@ -133,4 +133,21 @@
     tipoSanguineo.text = [listaTpSanguineo objectAtIndex:row];
 }
 
+#pragma mark - UITextFieldDelegate
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField;
+{
+    NSInteger nextTag = textField.tag + 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [textField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [textField resignFirstResponder];
+    }
+    return NO; // We do not want UITextField to insert line-breaks.
+}
+
 @end
