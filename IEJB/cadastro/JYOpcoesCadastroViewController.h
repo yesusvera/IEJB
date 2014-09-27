1b2b98379abdd2905b3f77c604797baae539c063
zaganin <jj.salcedo@hotmail.com>
2014-09-26 23:15:51 -0300

    Tentativa de incluir a Top Bar na Table view controler

--- IEJB/cadastro/JYOpcoesCadastroViewController.h
+++ IEJB/cadastro/JYOpcoesCadastroViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JYOpcoesCadastroViewController : UIViewController
+@interface JYOpcoesCadastroViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
 
 @end
--- IEJB/cadastro/JYOpcoesCadastroViewController.m
+++ IEJB/cadastro/JYOpcoesCadastroViewController.m
@@ -26,6 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view.
 }
 
@@ -34,6 +35,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 /*
 #pragma mark - Navigation
