28325fcf4f8d1e0cd11948eea09d7d1d23fe0d06
zaganin <jj.salcedo@hotmail.com>
2014-09-27 21:48:16 -0300

    Iniciando o montador de UF cidade

---
+++ IEJB/UfCidadeDao.h
@@ -0,0 +1,13 @@
+//
+//  UfCidadeDao.h
+//  IEJB
+//
+//  Created by Jonathan Jordan Carrillo Salcedo on 27/09/14.
+//  Copyright (c) 2014 JY DESENV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UfCidadeDao : NSObject
+
+@end
---
+++ IEJB/UfCidadeDao.m
@@ -0,0 +1,30 @@
+//
+//  UfCidadeDao.m
+//  IEJB
+//
+//  Created by Jonathan Jordan Carrillo Salcedo on 27/09/14.
+//  Copyright (c) 2014 JY DESENV. All rights reserved.
+//
+
+#import "UfCidadeDao.h"
+
+@implementation UfCidadeDao
+
+
+//- (NSDictionary)loginValido:(NSString *)usuario senha:(NSString *)senha{
+//    NSArray *results = [SCSQLite selectRowSQL:@"Select * from tbl_login where usuario = '%@' and senha = '%@'", usuario, senha];
+//    return results.count > 0;
+//    
+//    
+//    _itemId = dictionary[@"id"];
+//    16.    _name = dictionary[@"name"];
+//    17.    NSArray *tags = dictionary[@"tags"];
+//    18.    NSMutableArray *tagsArray = [[NSMutableArray alloc]
+//                                        initWithCapacity:tags.count];
+//    19.    for (NSDictionary *tag in tags) {
+//        20.     [tagsArray addObject:tag[@"name"]];
+//        21.    }
+//    22.    _tagsArray = [tagsArray copy];
+}
+
+@end
