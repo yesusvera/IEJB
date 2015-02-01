733b6b44c2d61e96800271a1e7fef45338a846b6
zaganin <jj.salcedo@hotmail.com>
2015-02-01 14:01:06 -0200

    Atualizando interface

--- IEJB/JYTiposCadastros.h
+++ IEJB/JYTiposCadastros.h
@@ -5,8 +5,6 @@
 //  Created by Jonathan Jordan Carrillo Salcedo on 10/10/14.
 //  Copyright (c) 2014 JY DESENV. All rights reserved.
 //
-
-//#import <Foundation/Foundation.h>
 #import "XLFormViewController.h"
 
 @interface JYTiposCadastros : XLFormViewController
